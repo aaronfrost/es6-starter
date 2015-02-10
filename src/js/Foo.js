0604524bf46d8b57336dae336978455e61facd03
Aaron-Frost <frost@domo.com>
2015-02-10 05:44:32 -0700

    init

---
+++ src/js/Foo.js
@@ -0,0 +1,12 @@
+class Foo{
+  constructor(){
+    console.log("Instantiate Foo");
+  }
+  bar(){
+    console.log("Foo");
+  }
+
+}
+
+
+export default Foo;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: './src/js/app'
+  },
+
+  output: {
+    filename: '[name].js',
+    path: 'public/js',
+    chunkFilename: '[id].chunk.js',
+    publicPath: '/'
+  },
+
+  module: {
+    loaders: [
+      { test: /src(.+)\.js$/, loader: '6to5-loader?runtime&experimental' }
+    ]
+  },
+
+  devtool: 'inline-source-map',
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('commons.js'),
+    new webpack.ProvidePlugin({
+      to5Runtime: 'imports?global=>{}!exports?global.to5Runtime!6to5/runtime'
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+
+};
