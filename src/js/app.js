8c94eb1eef9e5618b49a0035988ffe28bc6943fd
Aaron-Frost <frost@domo.com>
2015-02-10 09:41:38 -0700

    Added Let Stuff

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,310 @@
-//Import external files using module syntax
-import Foo from './Foo'; //If it's a file of your own, use the ./ at the beginning of the filename
-import _ from 'lodash'; //Otherwise, use the npm name
-
-//Once imported, use your imports
-let foo = new Foo();  //logs 'instantiate Foo'
-foo.bar();            //logs 'bar'
-
-console.log(_.uniq([1, 2, 3, 4, 1, 4])); //Logs [1, 2, 3, 4]
+/*
+  Variable Hoisting
+  LET
+  CONST
+  Block Functions
+ */
 
 
-function factorial(n, acc = 1) {
-  "use strict";
-  if (n <= 1) return acc;
-  return factorial(n - 1, n * acc);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //BASIC LET
+let a = 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// REAssign It
+//let b = 2;
+//b = 3;
+//console.log('b',b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//IF scope
+//let c = 2;
+//if(true){
+//  let bar = 1;
+//}
+//console.log( c + bar ); //ERROR
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Same Name, Nested Scope: OK
+//let d = 0;
+//if(true){
+//  let d = 2;
+//  console.log("here d = ",d);     //Logs 2
+//}
+//console.log("at the end d = ",d); //Logs 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Same Name, Same Scope: OK
+//let d = 0;
+//let d = 2;  //Works in 6to5, but not legal in ES6 spec
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//LET SCOPES
+
+//let a = 0;      //BASIC
+//
+//if(true){       //IF
+//  let b = 1;
+//}
+//
+//let term = "js";
+//switch(term){   //SWITCH
+//  case "js":
+//    let c = 2;
+//    break;
+//}
+//
+//while(true){    //LOOP BODY
+//  let d = 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TEMPORAL DEAD ZONE
+function doSomething(){
+  console.log(a);  //What Here?
+  let a = 1;
+  console.log(a);
 }
+//Memory is reserved
+//But variable isn't accessible
+//6to5 allows for it, but ES6 doesn't
 
-// Stack overflow in most implementations today,
-// but safe on arbitrary inputs in eS6
-console.log(factorial(100000));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//LET AS COUNTER
+
+////VAR I
+//for(var i = 0; i < 10; i++){
+//  //console.log(i); //Logs 0-9
+//}
+//
+////LET J
+//for(let j = 0; j < 10; j++){
+//  //console.log(j); //Logs 0-9
+//}
+//console.log(i); //Logs 10
+//console.log(j); //ReferenceError
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//CONST
+//const e = 0;
+//e = 1;  //SyntaxError: Assignment to constant variable
+// 6to5 won't even compile this
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//BLOCKS
+//{
+//  let something = "another thing";
+//  console.log('inblock',something);
+//}
+//console.log('outblock',something);
+
+//arbitrary block
+//Been in JS for a long time, but no one cared until LET and CONST
+
+
